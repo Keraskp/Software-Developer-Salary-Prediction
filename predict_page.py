defe5da27c1a1dc13dcd47afa0282478b6b90f75
Aditya Kiran Pal <suggi.aditya@gmail.com>
2022-06-30 03:45:15 +0530

    Modified Model

--- predict_page.py
+++ predict_page.py
@@ -41,4 +41,4 @@ def show_predict_page(): #Here we build our streamlit app
 		X = X.astype(float)
 
 		salary = regressor.predict(X)
-		st.subheader(f"The estimated salary is ${salary[0][0]:.2f}")
+		st.subheader(f"The estimated salary is ${salary[0]:.2f}")
